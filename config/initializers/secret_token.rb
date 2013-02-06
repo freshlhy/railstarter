29dbc8de3b8d20ed214688852535aed2e4465e4e
liuhuyu <735077491@qq.com>
2013-02-06 22:53:43 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstarter::Application.config.secret_token = 'd8120d7efa46dc77e5ee84bd2075a2ce9f5abcc2d9752cd2effd51e55cb0100b867b57130f6c9e691573205dceec75dea04d67ab9bedfdd6850256fec2e9b1d1'
